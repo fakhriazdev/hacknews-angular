0a5d13070d5acf5c811a405ba7df1c6c985697a6
Muhammad Fakhri Aziz <fakhriazdev@gmail.com>
2024-02-23 23:05:44 +0700

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      black: '#242424',
+      white: '#fff',
+      orange: '#FF6600'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
